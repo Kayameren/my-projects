66fb405a74287ff436965d04839260a9f55dd9b1
Mustafa Eren Kaya <112939431+Kayameren@users.noreply.github.com>
2022-09-07 11:46:43 +0300

    Collatz Problem
    You can find out what the collatz problem is from this link:  https://en.wikipedia.org/wiki/Collatz_conjecture

---
+++ collatz_problem.py
@@ -0,0 +1,31 @@
+
+
+"If you want to learn about the collatz problem, you can find it here https://en.wikipedia.org/wiki/Collatz_conjecture"
+
+
+
+while True:
+    try:
+        x=int(input("x (positive and integer): "))
+        if x<=0:
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print("please enter a positive and integer")
+
+y=0
+
+while True:
+    if x==1:
+        print(f"1 found after {y} transactions")
+        break
+    if x%2==0:
+        x=x/2
+        print(x)
+        y+=1
+    else:
+        x=3*x+1
+        print(x)
+        y+=1
+    
